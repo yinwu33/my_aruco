487a0c7c7851d1e26b0ac397a2f78b1d5fb75e8a
Yin Wu <yinwu.life@gmail.com>
2022-03-07 15:23:29 +0100

    optim not finished

--- src/front_optim.cpp
+++ src/front_optim.cpp
@@ -5,21 +5,32 @@ namespace my_aruco
 
 struct CostFunction {
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-    CostFunction(const Eigen::Quaterniond& Q12, const Eigen::Vector3d& t12)
-    : Q12_(Q12), t12_(t12) { }
+    CostFunction(
+      const Eigen::Quaterniond& Q1,
+      const Eigen::Vector3d& t1,
+      const Eigen::Quaterniond& Q2,
+      const Eigen::Vector3d& t2,
+      const Eigen::Quaterniond& Q12,
+      const Eigen::Vector3d& t12)
+    : Q1_(Q1), t1_(t1), Q2_(Q2), t2_(t2), Q12_(Q12), t12_(t12) { }
 
   template <typename T>
-  bool operator()(const T* const x, T* residual) const {
+  bool operator()(const T* const dQ1, const T* const dt1, const T* const dQ2, const T* const dt2, T* residual) const {
+    T dQ1_w = dQ1[-]
     return true;
   }
 
   static ceres::CostFunction* Create(const Eigen::Vector3d measuremnt) {
-    return (new ceres::AutoDiffCostFunction<CostFunction, 4, 1, 1>(new CostFunction()));
+    return (new ceres::AutoDiffCostFunction<CostFunction, 4, 4, 3, 4, 3>(new CostFunction()));
   }
 
 public:
   Eigen::Quaterniond Q12_;
+  Eigen::Quaterniond Q1_;
+  Eigen::Quaterniond Q2_;
   Eigen::Vector3d t12_;
+  Eigen::Vector3d t1_;
+  Eigen::Vector3d t2_;
 
 };
 
